a041384e490767d14a3594e9070e2cf99d3ef95c
HoangLe87 <hongacek@hotmail.com>
2023-02-03 20:51:36 +0100

    incrementor completed

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -6,19 +6,30 @@ use ink_lang as ink;
 mod incrementer {
 	#[ink(storage)]
 	pub struct Incrementer {
-		// Storage Declaration
+		value:i32,
 	}
 
 	impl Incrementer {
 		#[ink(constructor)]
 		pub fn new(init_value: i32) -> Self {
 			// Contract Constructor
-			Self {}
+			Self {value: init_value,}
+		}
+
+		#[ink(constructor)]
+		pub fn default() -> Self {
+			// Contract Constructor
+			Self {value: 0,}
 		}
 
 		#[ink(message)]
-		pub fn get(&self) {
-			// Contract Message
+		pub fn get(&self)->i32 {
+			self.value
+		}
+
+		#[ink(message)]
+		pub fn inc(&mut self, by: i32) {
+			self.value +=by
 		}
 	}
 
@@ -26,10 +37,20 @@ mod incrementer {
 	mod tests {
 		use super::*;
 		use ink_lang as ink;
-
 		#[ink::test]
 		fn default_works() {
-			// Test Your Contract
+			let contract = Incrementer::default();
+			assert_eq!(contract.get(),0)
 		}
+		
+		#[ink::test]
+		fn it_works() {
+			let mut contract = Incrementer::new(42);
+			assert_eq!(contract.get(), 42);
+			contract.inc(5);
+			assert_eq!(contract.get(), 47);
+			contract.inc(-50);
+			assert_eq!(contract.get(), -3);
 	}
+}
 }
